17a295a144f9b4d3597014fdfeb31c0912ffc568
Andrey Pereshein <109091740+Refygiym@users.noreply.github.com>
2022-08-29 12:59:20 +0300

    Add files via upload

---
+++ Home work 1/script.js
@@ -0,0 +1,7 @@
+'use strict';
+const operandA = +prompt ('OperandA');
+const operandB = +prompt ('OperandB');
+
+const result = operandA + operandB;
+
+alert(operandA + '+' + operandB+ '=' + result);
