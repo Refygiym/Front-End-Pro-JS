5d908e128dfc038b1dc49a7de01817b5a37a1d22
Andrey Pereshein <109091740+Refygiym@users.noreply.github.com>
2022-09-22 13:54:00 +0300

    Add files via upload

---
+++ Home work 7/script.js
@@ -0,0 +1,11 @@
+function createCalculator(base) {
+    return {
+        add: (b) => (base += b),
+        div: (b) => (base /= b),
+        mult: (b) => (base *= b),
+        sub: (b) => (base -= b),
+        set: (b) => (base = b),
+    };
+}
+
+const calc = createCalculator(100);
